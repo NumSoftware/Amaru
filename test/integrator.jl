4f7c70c5f90d37c3eef6b3543032fc0ac466bb34
yadian818 <yadian.menendez@gmail.com>
2018-11-14 16:32:37 -0200

    working 14/11

--- test/integrator.jl
+++ test/integrator.jl
@@ -5,23 +5,20 @@ mat = Orthotropic(E=31.72e6, nu=0.2, fc=30.68e3, ft=3.1e3, epsc=0.00218, epsu= 0
 int = MechIntegrator(mat)
 
 nu = 0.2
-#Δε = [ 0.0025*nu, 0.0025*nu, -0.0025, 0, 0, 0 ]
-#Δε = [ 0.0000, 0.0000, -0.0033, 0, 0, 0 ]*-0.025
-#stress_update(int, Δε, nincs=20)
+euu= -0.0033
 
-Δε = [ 0.0000, 0.0000, -0.0033, 0, 0, 0 ]*0.85
-stress_update(int, Δε, nincs=30)
+Δε = [ euu*nu, euu*nu, euu, 0, 0, 0 ]*0.85
+stress_update(int, Δε, nincs=100)
 
-Δε = [ 0.0000, 0.0000, -0.0033, 0, 0, 0 ]*-0.450
-stress_update(int, Δε, nincs=30)
+Δε = [ euu*nu, euu*nu, euu, 0, 0, 0 ]*-0.45
+stress_update(int, Δε, nincs=100)
 
-Δε = [ 0.0000, 0.0000, -0.0033, 0, 0, 0 ]*0.55
-stress_update(int, Δε, nincs=30)
+Δε = [ euu*nu, euu*nu, euu, 0, 0, 0 ]*0.55
+stress_update(int, Δε, nincs=100)
 
 using PyPlot
 t = int.table
 grid("on", linewidth=0.5, color="lightgray", linestyle="-")     # propriedades do grid
 plot(t[:ezz], t[:szz], "-o")
 tight_layout()                                                  # centrar a figura
-#@show t
-#;
+
